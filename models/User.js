e354ab5a2609dcff1dfec45a96c633eb7120a1e6
Junaid SIkander Hakro <Junaid_sikander@live.com>
2020-09-16 21:56:43 +0500

    [ADD] hashing password before save  and compare password

--- models/User.js
+++ models/User.js
@@ -20,4 +20,27 @@ const UserSchema = new mongoose.Schema({
     todos : [{type: mongoose.Schema.ObjectId, ref: 'Todo'}]
 });
 
+UserSchema.pre('save', function (next){
+    if(!this.isModified('password'))
+        return next();
+    bcrypt.hash(this.password,10,(err, passwordHash) => {
+        if(err)
+            return next(err);
+        this.password = passwordHash;
+        next();
+    });
+});
+
+UserSchema.methods.comparePassword = function(password, cb){
+    bcrypt.compare(password,this.password, (err, isMatch) => {
+        if(err)
+            return cb(err);
+        else{
+            if(!isMatch)
+                return cb(null, isMatch);
+            return cb(null,this);
+        }
+    });
+};
+
 module.exports = mongoose.model('User',UserSchema);
