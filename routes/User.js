ca0f95f89326c425f65b897317a97f9d175d6222
Junaid SIkander Hakro <Junaid_sikander@live.com>
2020-09-17 01:01:34 +0500

    [ADD] GET todos request in userRouter

--- routes/User.js
+++ routes/User.js
@@ -63,4 +63,13 @@ userRouter.post('/todo', passport.authenticate('jwt', {session: false}),(req, re
     })
 });
 
+userRouter.get('/todos', passport.authenticate('jwt', {session: false}),(req, res) => {
+    User.findById({_id: req.user._id}).populate('todos').exec((err, document) => {
+        if(err)
+            res.status(500).json({message: {msgBody: "Error has occurred", msgError: true}});
+        else
+            res.status(200).json({todos: document.todos, authenticated: true});
+    })
+});
+
 module.exports = userRouter;
