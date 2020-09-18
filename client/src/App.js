8e3a25b8e0d6279e76f31cdd981ca715489cdf17
Junaid SIkander Hakro <Junaid_sikander@live.com>
2020-09-18 21:39:57 +0500

    [ADD] Home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 function App() {
     return (
         <Router>
           <Navbar/>
+          <Route exact path="/" component={Home} />
         </Router>
     );
 }
