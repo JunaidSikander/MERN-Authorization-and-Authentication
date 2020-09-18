4b65ee1d92c63751a068ca5f5c96090be623d400
Junaid SIkander Hakro <Junaid_sikander@live.com>
2020-09-18 16:38:20 +0500

    [ADD] AuthContext in Context folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React,{useContext} from 'react';
+import {AuthContext} from './Context/AuthContext';
 
 function App() {
+  const {user,setUser, isAuthenticated, setIsAuthenticated} = useContext(AuthContext);
+  console.log(user);
+  console.log(isAuthenticated);
   return (
     <p> Placeholder</p>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import AuthProvider from './Context/AuthContext.js'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'));
+    <AuthProvider>
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    </AuthProvider>,
+    document.getElementById('root'));
