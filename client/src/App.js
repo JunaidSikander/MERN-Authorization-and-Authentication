a337cc3350e92f87d131890a4e39c8dbd708df42
Junaid SIkander Hakro <Junaid_sikander@live.com>
2020-09-19 12:33:26 +0500

    [ADD] Register Component

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import Login from "./Components/Login";
+import Register from "./Components/Register";
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 function App() {
     return (
         <Router>
           <Navbar/>
           <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
+          <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
         </Router>
     );
 }
