93f191f250549d4b34b4cc8511e663c2b25559bb
Junaid SIkander Hakro <Junaid_sikander@live.com>
2020-09-19 12:31:37 +0500

    [FIX] classNaming mistake

--- client/src/Components/Message.js
+++ client/src/Components/Message.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
 const getStyle = (props) =>{
-    let baseClass = "alert";
+    let baseClass = "alert ";
     if(props.message.msgError)
-        baseClass = baseClass + "alert-danger";
+        baseClass = baseClass + "alert-danger ";
     else
-        baseClass = baseClass + "alert-primary";
+        baseClass = baseClass + "alert-primary ";
     return baseClass + "text-center"
 };
 
 const Message = props => {
     return(
-        <div className={getStyle(props)} role="alert">
+        <div className={getStyle(props)} role="alert ">
             {props.message.msgBody}
         </div>
     );
