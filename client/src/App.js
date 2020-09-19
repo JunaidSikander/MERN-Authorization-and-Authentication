583ac47c7adb59f502ab8ccf61017241ccdb87ad
Junaid SIkander Hakro <Junaid_sikander@live.com>
2020-09-19 11:55:19 +0500

    [ADD] Login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
+import Login from "./Components/Login";
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 function App() {
     return (
         <Router>
           <Navbar/>
           <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
         </Router>
     );
 }
