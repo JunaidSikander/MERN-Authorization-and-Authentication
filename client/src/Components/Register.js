e8548b57d4121e0612ebd3f931080c13d02be191
Junaid SIkander Hakro <Junaid_sikander@live.com>
2020-10-17 14:21:51 +0500

    [FIX] clear inputs when click on register

--- client/src/Components/Register.js
+++ client/src/Components/Register.js
@@ -44,18 +44,21 @@ const Register = props => {
                     <label htmlFor="username" className="sr-only">Username:</label>
                     <input type="text"
                            name="username"
+                           value={user.username}
                            onChange={onChange}
                            className="form-control"
                            placeholder="Enter Username"/>
                     <label htmlFor="password" className="sr-only">Password: </label>
                     <input type="password"
                            name="password"
+                           value={user.password}
                            onChange={onChange}
                            className="form-control"
                            placeholder="Enter Password"/>
                     <label htmlFor="role" className="sr-only">Role: </label>
                     <input type="text"
                            name="role"
+                           value={user.role}
                            onChange={onChange}
                            className="form-control"
                            placeholder="Enter Role"/>
